bcc5ce57a97c6bd43a2454723c3e0dbab2567314
Dennison Bertram <dennison@dennisonbertram.com>
2019-01-21 15:30:17 -0300

    Added new Truffle-Config file

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,39 @@
-'use strict';
+"use strict";
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic =
+  "Your Twelve Word Mnemonic";
 
 module.exports = {
   networks: {
     local: {
-      host: 'localhost',
+      host: "localhost",
       port: 9545,
       gas: 5000000,
       gasPrice: 5e9,
-      network_id: '*'
+      network_id: "*"
+    },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://mainnet.infura.io/v3/<<Your INFURA API TOKEN>>", 2
+        );
+      },
+      gas: 200000000,
+      gasPrice: 5e7,
+      network_id: 1
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://rinkeby.infura.io/<<Your INFURA API TOKEN>>"
+        );
+      },
+      gas: 5000000,
+      gasPrice: 5e9,
+      network_id: 3
     }
   }
 };
